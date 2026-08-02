f07d9b3f8c12eeaa067d22d53314d3c95b3274b1
Gift Nnko <nnkogift@gmail.com>
2026-08-02 17:08:11 +0300

    chore: added playground metadata

--- apps/playground/d2.config.js
+++ apps/playground/d2.config.js
@@ -1,7 +1,7 @@
 /** @type {import('@dhis2/cli-app-scripts').D2Config} */
 const config = {
     type: 'app',
-    title: 'Program Rule Playground',
+    title: 'Program Rules Playground',
     entryPoints: {
         app: './src/AppWrapper.tsx',
     },
